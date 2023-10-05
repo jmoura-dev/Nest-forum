6c376d5efb961cc9abd4aba36da71d4479f45e8d
Jackson <jackson1712@hotmail.com>
2023-10-05 15:31:02 -0300

    refactor: change items to receive new features and configs

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -7,6 +7,8 @@ import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prism
 import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
 import { PrismaQuestionsRepository } from './prisma/repositories/prisma-question-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
 
 @Module({
   providers: [
@@ -17,6 +19,10 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
     {
       provide: QuestionsRepository,
       useClass: PrismaQuestionsRepository,
+    },
+    {
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository,
     },
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
@@ -29,6 +35,7 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
     QuestionsRepository,
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
+    StudentsRepository,
   ],
 })
 export class DatabaseModule {}
