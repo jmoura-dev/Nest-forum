f0f333dc206f1b931041633f353e05fa36a0ddf3
Jackson <jackson1712@hotmail.com>
2023-10-16 19:52:36 -0300

    feat: add new features to value object and refactor files and tests

--- src/infra/database/prisma/repositories/prisma-answer-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answer-repository.ts
@@ -60,7 +60,7 @@ export class PrismaAnswersRepository implements AnswersRepository {
   async save(answer: Answer): Promise<void> {
     const data = PrismaAnswerMapper.toPrisma(answer)
 
-    Promise.all([
+    await Promise.all([
       this.prisma.answer.update({
         where: {
           id: data.id,
